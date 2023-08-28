0026c246e4cc564e90f7b6b11f0b3fd8f4776389
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-08-28 22:39:18 +0300

    Made all the test cases pass

--- test1.spec.js
+++ test1.spec.js
@@ -1,5 +1,5 @@
 describe("Testing The Workflows", () => {
   it("should pass when the sum is 2", () => {
-    expect(2 + 2).toBe(2);
+    expect(2 + 2).toBe(4);
   });
 });
--- test2.spec.js
+++ test2.spec.js
@@ -1,5 +1,5 @@
 describe("Testing The Workflows", () => {
   it("should pass when the sum is 2", () => {
-    expect(1 + 1).toBe(3);
+    expect(1 + 1).toBe(2);
   });
 });
